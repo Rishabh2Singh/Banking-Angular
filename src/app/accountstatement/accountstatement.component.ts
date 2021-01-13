b97de8f565092e74da21fe4a7762a74532bea4a7
Asra <mogalasra@gmail.com>
2021-01-13 16:13:30 +0530

    Login-Dashboard,account statement,details and summary front end

---
+++ src/app/accountstatement/accountstatement.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountStatementServiceService } from '../account-statement-service.service';
+
+@Component({
+  selector: 'app-accountstatement',
+  templateUrl: './accountstatement.component.html',
+  styleUrls: ['./accountstatement.component.css']
+})
+export class AccountstatementComponent implements OnInit {
+
+  stmt:any;
+  accountStatementCredentials: AccountStatementCredentials = new AccountStatementCredentials();
+ // accStat : AccountStatementDto[]=[];
+  constructor(private accountStatementService:AccountStatementServiceService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  accStatement(){
+    this.accountStatementService.fetchStatement(this.accountStatementCredentials).subscribe(
+      (response:any)=>{
+        this.stmt=response;
+  })
+  } 
+}
+export class AccountStatementCredentials {
+ customerId  =1356786291;
+ fromDate:Date =new Date();
+ toDate:Date =new Date();
+}
+
+export class AccountStatementDto{
+    name:string="" ;
+    accountNo:number=0;
+    amount:number=0;
+    type:string="";
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginDashboardComponent } from './login-dashboard/login-dashboard.component';
+import { AccountstatementComponent } from './accountstatement/accountstatement.component';
+import { AccountsummaryComponent } from './accountsummary/accountsummary.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AccountdetailsComponent } from './accountdetails/accountdetails.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
+const route : Routes = [
+{path : 'account-statement' , component : AccountstatementComponent},
+{path : 'account-summary' , component : AccountsummaryComponent},
+{path : 'account-details' , component : AccountdetailsComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDashboardComponent,
+    AccountstatementComponent,
+    AccountsummaryComponent,
+    AccountdetailsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
